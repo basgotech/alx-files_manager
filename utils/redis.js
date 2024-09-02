519ae539c6d412232ce34bcde80230df7cc115ca
Basliel Tegegn <basgolilo5@gmail.com>
2024-09-02 11:00:46 -0700

    fix

--- utils/redis.js
+++ utils/redis.js
@@ -1,62 +1,68 @@
-import redis from 'redis';
-import { promisify } from 'util';
+import redis
+from functools import partial
 
-/**
- * Class for performing operations with Redis service
- */
-class RedisClient {
-  constructor() {
-    this.client = redis.createClient();
-    this.getAsync = promisify(this.client.get).bind(this.client);
+class RedisClient:
+    """
+    Class for performing operations with Redis service
+    """
+    def __init__(self):
+        self.client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+        
+        # Register event handlers for Redis client
+        self.client.connection_pool.connection_kwargs['error_handler'] = self.error_handler
+        self.client.connection_pool.connection_kwargs['connect_handler'] = self.connect_handler
 
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
-    });
+    def error_handler(self, error):
+        print(f"Redis client not connected to the server: {error}")
 
-    this.client.on('connect', () => {
-      // console.log('Redis client connected to the server');
-    });
-  }
+    def connect_handler(self):
+        # print('Redis client connected to the server')
+        pass
 
-  /**
-   * Checks if connection to Redis is Alive
-   * @return {boolean} true if connection alive or false if not
-   */
-  isAlive() {
-    return this.client.connected;
-  }
+    def is_alive(self):
+        """
+        Checks if connection to Redis is alive
+        :return: True if connection is alive, False otherwise
+        """
+        try:
+            self.client.ping()
+            return True
+        except redis.ConnectionError:
+            return False
 
-  /**
-   * gets value corresponding to key in redis
-   * @key {string} key to search for in redis
-   * @return {string}  value of key
-   */
-  async get(key) {
-    const value = await this.getAsync(key);
-    return value;
-  }
+    async def get(self, key):
+        """
+        Gets value corresponding to key in Redis
+        :param key: key to search for in Redis
+        :return: value of key or None if not found
+        """
+        try:
+            value = await self.client.get(key)
+            return value
+        except Exception as e:
+            print(f"Error fetching key '{key}': {e}")
+            return None
 
-  /**
-   * Creates a new key in redis with a specific TTL
-   * @key {string} key to be saved in redis
-   * @value {string} value to be asigned to key
-   * @duration {number} TTL of key
-   * @return {undefined}  No return
-   */
-  async set(key, value, duration) {
-    this.client.setex(key, duration, value);
-  }
+    async def set(self, key, value, duration):
+        """
+        Creates a new key in Redis with a specific TTL
+        :param key: key to be saved in Redis
+        :param value: value to be assigned to the key
+        :param duration: TTL of the key in seconds
+        """
+        try:
+            await self.client.setex(key, duration, value)
+        except Exception as e:
+            print(f"Error setting key '{key}' with value '{value}': {e}")
 
-  /**
-   * Deletes key in redis service
-   * @key {string} key to be deleted
-   * @return {undefined}  No return
-   */
-  async del(key) {
-    this.client.del(key);
-  }
-}
+    async def delete(self, key):
+        """
+        Deletes key in Redis service
+        :param key: key to be deleted
+        """
+        try:
+            await self.client.delete(key)
+        except Exception as e:
+            print(f"Error deleting key '{key}': {e}")
 
-const redisClient = new RedisClient();
-
-export default redisClient;
+redis_client = RedisClient()
